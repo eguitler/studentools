b10946ceada7197b7487c7906a7056adac210070
Ezequiel Guitler <eguitler@gmail.com>
2016-07-03 16:22:45 -0300

    Fix import errors

--- studentools/urls.py
+++ studentools/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
@@ -25,5 +25,5 @@ urlpatterns = [
     # Home
     url(r'^$', Home.as_view()),
     # Manage Account
-    url(r'^accounts/', registration.backends.hmac.urls),
+    url(r'^accounts/', include('registration.backends.hmac.urls')),
 ] + staticfiles_urlpatterns()
