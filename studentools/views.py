c6d98968c1ef138b2290b44949cef50b40314c64
Ezequiel Guitler <eguitler@gmail.com>
2016-07-16 16:24:33 -0300

    check in home view if user is authenticated

--- studentools/views.py
+++ studentools/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import View
+from django.contrib.auth.views import login
+
+from  studentools.forms import (
+    SignUpForm,
+    LogInForm
+)
 
 class Home(View):
 
     def get(self, request):
         context = {}
-        return render(request, 'index.html', context)
+        if request.user.is_authenticated():
+            return render(request, 'index.html', context)
+        else:
+            return login(request, authentication_form=LogInForm)
+
+    def post(self, request):
+        return login(request, authentication_form=LogInForm)
 
 
 class Institutions(View):
