f2bd1c45983a9f0f94d902d75aaf741c48d6d3ba
Ezequiel Guitler <eguitler@gmail.com>
2016-07-16 16:21:39 -0300

    LogInForm added

--- studentools/forms.py
+++ studentools/forms.py
@@ -1,5 +1,12 @@
 from django import forms
 
+from django.contrib.auth.forms import AuthenticationForm
+
+class LogInForm(AuthenticationForm):
+    username = forms.CharField(min_length=6, strip=True, label='', widget=forms.TextInput(attrs={'placeholder':'Username'}))
+    password = forms.CharField(min_length=6, label='', widget=forms.PasswordInput(attrs={'placeholder':'Password'}))
+
+
 class SignUpForm(forms.Form):
     username = forms.CharField(min_length=6, strip=True)
     email = forms.EmailField()
