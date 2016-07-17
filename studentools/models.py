fcc1c33312843080c0bafe69253300bb418bc52a
Ezequiel Guitler <eguitler@gmail.com>
2016-07-17 18:20:09 -0300

    User Models added

--- studentools/models.py
+++ studentools/models.py
@@ -1,2 +1,30 @@
 from django.db import models
 from django.contrib.auth.models import User
+
+
+class BaseCustomUser(User):
+    pass
+
+
+class HumanUser(BaseCustomUser):
+    pass
+
+
+class EntityUser(BaseCustomUser):
+    pass
+
+
+class Institution(EntityUser):
+    pass
+
+
+class Headquarter(EntityUser):
+    pass
+
+
+class Teacher(HumanUser):
+    pass
+
+
+class Student(HumanUser):
+    pass
