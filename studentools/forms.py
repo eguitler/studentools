2f7a0f61a9636f05c7584ec5ee01f69ba5e97bc2
Ezequiel Guitler <eguitler@gmail.com>
2016-07-17 18:17:15 -0300

    Login & registration forms were updated

--- studentools/forms.py
+++ studentools/forms.py
@@ -1,14 +1,60 @@
-from django import forms
+from datetime import datetime
 
+from django import forms
 from django.contrib.auth.forms import AuthenticationForm
+from registration.forms import RegistrationForm
+#from registration.forms import RegistrationFormUniqueEmail
+from django.contrib.auth.models import User
+
+from studentools.models import CustomUser
+
+
+year = datetime.now().year
+BIRTH_YEARS = sorted(range(year-100, year+1), reverse=True)
+
+CHOICES = (
+    ('M', 'Male'),
+    ('F', 'Female')
+)
+
+def ph_email(msg):
+    return forms.EmailInput(attrs={'placeholder': msg}) 
+
+def ph_pass(msg):
+    return forms.PasswordInput(attrs={'placeholder': msg})
+
+def ph_text(msg):
+    return forms.TextInput(attrs={'placeholder': msg})
+
+def set_placeholders(form):
+    for field in form.fields.values():
+        field.required = True
+        if field.label:
+            field.widget.attrs['placeholder'] = field.label
+        else:
+            field.widget.attrs['placeholder'] = field.help_text.capitalize()
 
 class LogInForm(AuthenticationForm):
-    username = forms.CharField(min_length=6, strip=True, label='', widget=forms.TextInput(attrs={'placeholder':'Username'}))
-    password = forms.CharField(min_length=6, label='', widget=forms.PasswordInput(attrs={'placeholder':'Password'}))
 
+    class Meta():
+        model = CustomUser
+        fields = ('username', 'password')
 
-class SignUpForm(forms.Form):
-    username = forms.CharField(min_length=6, strip=True)
-    email = forms.EmailField()
-    password = forms.CharField(min_length=6, widget=forms.PasswordInput)
-    repassword = forms.CharField(min_length=6, widget=forms.PasswordInput)
+    def __init__(self, *args, **kwargs):
+        super(LogInForm, self).__init__(*args, **kwargs)
+        set_placeholders(self)
+
+# In production must have unique email
+#class SignUpForm(RegistrationFormUniqueEmail):
+class SignUpForm(RegistrationForm):
+
+    class Meta():
+        model = CustomUser
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
+
+    def __init__(self, *args, **kwargs):
+        super(SignUpForm, self).__init__(*args, **kwargs)
+        set_placeholders(self)
+
+    birthday = forms.DateField(required=True, widget=forms.widgets.SelectDateWidget(years=BIRTH_YEARS))
+    genere = forms.ChoiceField(required=True, label='', choices=CHOICES, widget=forms.RadioSelect)
