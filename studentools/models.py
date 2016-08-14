7968a6f52a072816f546b22cf6218b3803c244cd
Ezequiel Guitler <eguitler@gmail.com>
2016-08-13 22:23:35 -0300

    models created

--- studentools/models.py
+++ studentools/models.py
@@ -2,29 +2,80 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class EntityModel(models.Model):
+    tag = models.CharField(max_length=10, default=None)
+    name = models.CharField(max_length=50, default=None)
+    email = models.EmailField(default=None)
+    phone = models.CharField(max_length=15, default=None)
+    address = models.CharField(max_length=50, default=None)
+    latitude = models.FloatField(default=None)
+    longitude = models.FloatField(default=None)
+    web = models.URLField(default=None)
+    social_web = models.URLField(default=None)
+    #image = models.ImageField(upload_to=None)
+
+
+class University(EntityModel):
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Universities'
+
+
+class Faculty(EntityModel):
+    university_parent = models.ForeignKey( 'University',
+                                            on_delete=models.CASCADE,
+                                            related_name='faculties',
+                                            blank=True
+                        )
+    def __str__(self):
+        return '{} - {}'.format(self.tag, self.name)
+
+    class Meta:
+        verbose_name_plural = 'Faculties'
+
+
+class Institute(EntityModel):
+
+    def __str__(self):
+        return self.name
+
+class School(EntityModel):
+
+    def __str__(self):
+        return self.name
+
+
+# ======================================
+
 class BaseCustomUser(User):
-    pass
+
+    # first_name
+    # last_name
+    # email
+    # username
+    # password
+    birth_date = models.DateField(default=None)
+    genere = models.CharField(max_length=10, default=None)
+    age = models.PositiveSmallIntegerField(default=None)
+    phone = models.CharField(max_length=15, default=None)
+    address = models.CharField(max_length=50, default=None)
+    web = models.URLField(default=None)
+    social_web = models.URLField(default=None)
+    #image = models.ImageField(upload_to=None)
 
 
-class HumanUser(BaseCustomUser):
-    pass
+class Teacher(BaseCustomUser):
+    working_at = models.ManyToManyField( 'EntityModel',
+                                         related_name='teachers',
+                                         blank=True
+                 )
 
 
-class EntityUser(BaseCustomUser):
-    pass
-
-
-class Institution(EntityUser):
-    pass
-
-
-class Headquarter(EntityUser):
-    pass
-
-
-class Teacher(HumanUser):
-    pass
-
-
-class Student(HumanUser):
-    pass
+class Student(BaseCustomUser):
+    studying_at = models.ManyToManyField( 'EntityModel',
+                                          related_name='students',
+                                          blank=True
+                  )
